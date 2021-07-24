e223de753a9450f58a9a8fb2bfede003c89d1c70
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-07-24 14:45:39 +0530

    Added documentation

--- engine.py
+++ engine.py
@@ -7,8 +7,7 @@ from itertools import cycle
 
 class Cypher:
     def __init__(self):
-        self.symbols = deque(sorted(string.ascii_letters+string.digits+string.punctuation+' '))
-        self.order = [ord(i) for i in self.symbols]
+        self.__symbols__ = deque(sorted(string.ascii_letters+string.digits+string.punctuation+' '))
 
     def __str__(self):
         return """
@@ -32,7 +31,7 @@ class Cypher:
             Key - <int> type key required for encryption or decryption
 
         ROT13 Cypher
-            About - Procedure includes shifting the dictionary by a value of 13.
+            About - Procedure includes shifting the dictionary by the value of 13.
             Strength - Low
             Key - No key required for encryption or decryption
 
@@ -70,75 +69,157 @@ class Cypher:
             About - Procedure includes modifying xor cypher with base64 encoding scheme.
             Strength - High
             Key - <str> type key required for encryption or decryption
+
+        Note - Key provided/generated during encryption should be used for a successful decryption.
         """
 
+    def __engines__(self):
+        return [x for x in dir(self) if not x.startswith('__')]
 
-    def encrypt(self, message, engine, key=None):
+    def encrypt(self, message='hello world', engine='reverse', key=None):
+        """encrypts message attribute with the specified engine
+
+        Args:
+            message (str, optional): message to be encrypted. Defaults to 'hello world'.
+            engine (str, optional): engine to be used for encryption. Defaults to 'reverse'.
+            key ([int, float, str], optional): key used for encryption. Defaults to None.
+
+        Returns:
+           dict: dictionary with encrypted message, key and the engine used.
+        """
         encrypted = eval("self."+engine)(message=message, encrypt=True, decrypt=False, key=key)
         return {'encrypted_message': encrypted[0], 'key': encrypted[1], 'engine': engine}
     
-    def decrypt(self, message, engine, key=None):
+    def decrypt(self, message='dlrow olleh', engine='reverse', key=None):
+        """decrypts message attribute with the specified engine
+
+        Args:
+            message (str, optional): message to be decrypted. Defaults to 'dlrow olleh'.
+            engine (str, optional): engine to be used for decryption. Defaults to 'reverse'.
+            key ([int, float, str], optional): key used for decryption. Defaults to None.
+
+        Returns:
+           dict: dictionary with decrypted message, key and the engine used.
+        """
         decrypted = eval("self."+engine)(message=message, encrypt=False, decrypt=True, key=key)
         return {'decrypted_message': decrypted[0], 'key': decrypted[1], 'engine': engine}
 
 
-    def reverse(self, message, encrypt=False, decrypt=False, key=None):
+    def reverse(self, message: str, encrypt=False, decrypt=False, key=None):
+        """Procedure includes reversing the message.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (None, optional): No key required for encryption or decryption. Defaults to None.
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
+        if type(message) != str:
+            raise TypeError("'str' object required as 'message' attribute for encryption or decryption")
+
         return message[::-1], key
 
-    def caeser(self, message, encrypt=False, decrypt=False, key=None):
+    def caeser(self, message: str, encrypt=False, decrypt=False, key=None):
+        """Procedure includes shifting the dictionary.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (int): <int> type key required for encryption or decryption. Defaults to randint(1, 10000).
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
         if type(key) not in [int, float]:
             warnings.warn("int based key is preferred for ceaser cypher. Assuming random key.")
             key = random.randint(1, 10000)
 
-        rotated_symbols = self.symbols.copy()
+        rotated_symbols = self.__symbols__.copy()
         rotated_symbols.rotate(-int(key))
 
         if encrypt:                
-            encrypted_data = ''.join([rotated_symbols[self.symbols.index(sym)] for sym in message])
+            encrypted_data = ''.join([rotated_symbols[self.__symbols__.index(sym)] for sym in message])
             return encrypted_data, int(key)
 
         if decrypt:
-            decrypted_data = ''.join([self.symbols[rotated_symbols.index(sym)] for sym in message])
+            decrypted_data = ''.join([self.__symbols__[rotated_symbols.index(sym)] for sym in message])
             return decrypted_data, int(key)
     
-    def shifting_caeser(self, message, encrypt=False, decrypt=False, key=None):
+    def shifting_caeser(self, message: str, encrypt=False, decrypt=False, key=None):
+        """Procedure includes shifting the dictionary continously.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (int): <int> type key required for encryption or decryption. Defaults to randint(1, 10000).
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
         if type(key) not in [int, float]:
             warnings.warn("int based key is preferred for shifting ceaser cypher. Assuming random key.")
             key = random.randint(1, 10000)
         
         if encrypt:
-            rotated_symbols = self.symbols.copy()
+            rotated_symbols = self.__symbols__.copy()
             encrypted_data = ''
 
             for sym in message:
                 rotated_symbols.rotate(-int(key))
-                encrypted_data += rotated_symbols[self.symbols.index(sym)]
+                encrypted_data += rotated_symbols[self.__symbols__.index(sym)]
 
             return encrypted_data, int(key)
 
         if decrypt:
-            rotated_symbols = self.symbols.copy()
+            rotated_symbols = self.__symbols__.copy()
             decrypted_data = ''
 
             for sym in message:
                 rotated_symbols.rotate(-int(key))
-                decrypted_data += self.symbols[rotated_symbols.index(sym)]
+                decrypted_data += self.__symbols__[rotated_symbols.index(sym)]
 
             return decrypted_data, int(key)
     
-    def rot13(self, message, encrypt=False, decrypt=False, key=None):
-        rotated_symbols = self.symbols.copy()
+    def rot13(self, message: str, encrypt=False, decrypt=False, key=None):
+        """Procedure includes shifting the dictionary by the value of 13.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (None, optional): No key required for encryption or decryption.
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
+        rotated_symbols = self.__symbols__.copy()
         rotated_symbols.rotate(-13)
 
         if encrypt:                
-            encrypted_data = ''.join([rotated_symbols[self.symbols.index(sym)] for sym in message])
+            encrypted_data = ''.join([rotated_symbols[self.__symbols__.index(sym)] for sym in message])
             return encrypted_data, 13
 
         if decrypt:
-            decrypted_data = ''.join([self.symbols[rotated_symbols.index(sym)] for sym in message])
+            decrypted_data = ''.join([self.__symbols__[rotated_symbols.index(sym)] for sym in message])
             return decrypted_data, 13
 
-    def transposition(self, message, encrypt=False, decrypt=False, key=None):
+    def transposition(self, message: str, encrypt=False, decrypt=False, key=None):
+        """About - Procedure includes arranging the message row-wise, in a matrix and extracting column-wise.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (int): <int> type key required for encryption or decryption. Defaults to randint(1, len(message)).
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
         if (not key) or (type(key) not in [int, float]) or (int(key) >= len(message)):
             warnings.warn("int based key (len(key) <= len(message)) is preferred for shifting transposition cypher. Assuming random key.")
             key = random.randint(1, len(message))
@@ -161,7 +242,18 @@ class Cypher:
 
             return ''.join(decrypted_data), int(key)
 
-    def base64(self, message, encrypt=False, decrypt=False, key=None):
+    def base64(self, message: str, encrypt=False, decrypt=False, key=None):
+        """About - Procedure includes standard text-to-binary encoding scheme.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (None, optional): No key required for encryption or decryption.
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
         if encrypt:
             encrypted_data = base64.b64encode(message.encode('ascii'))
             return encrypted_data.decode('ascii'), key
@@ -169,10 +261,21 @@ class Cypher:
             decrypted_data = base64.b64decode(message.encode('ascii'))
             return decrypted_data.decode('ascii'), key
 
-    def xor(self, message, encrypt=False, decrypt=False, key=None):
+    def xor(self, message: str, encrypt=False, decrypt=False, key=None):
+        """About - Procedure includes applying xor operator on message and key..
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (str): <str> type key required for encryption or decryption. Defaults to random passkey.
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
         if (not key) or (type(key) != str):
             warnings.warn("str based key is preferred for xor cypher. Assuming random key.")
-            key = ''.join([random.choice(self.symbols) for _ in range(len(message))])
+            key = ''.join([random.choice(self.__symbols__) for _ in range(len(message))])
 
         if encrypt:
             data = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in zip(message, cycle(key)))
@@ -184,30 +287,52 @@ class Cypher:
             decrypted_data = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in zip(data, cycle(key)))
             return decrypted_data, key
     
-    def multiplicative(self, message, encrypt=False, decrypt=False, key=None):
+    def multiplicative(self, message: str, encrypt=False, decrypt=False, key=None):
+        """About - Procedure includes modifying caeser cypher with multiplication operation.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (int): <int> type key required for encryption or decryption. Defaults to randint(1, 10000).
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
         if type(key) not in [int, float]:
             warnings.warn("int based key is preferred for multiplicative cypher. Assuming random key.")
             key = random.randint(1, 10000)
 
         if encrypt:
-            encrypted_data = [self.symbols[(self.symbols.index(sym)*int(key))%len(self.symbols)] for sym in message]
+            encrypted_data = [self.__symbols__[(self.__symbols__.index(sym)*int(key))%len(self.__symbols__)] for sym in message]
             return ''.join(encrypted_data), int(key)
         
         if decrypt:
             decrypted_data = ''
             for sym in message:
-                for og_sym in self.symbols:
-                    if self.symbols[(self.symbols.index(og_sym)*int(key))%len(self.symbols)] == sym:
+                for og_sym in self.__symbols__:
+                    if self.__symbols__[(self.__symbols__.index(og_sym)*int(key))%len(self.__symbols__)] == sym:
                         decrypted_data += og_sym
 
             return decrypted_data, int(key)
 
-    def monoalphabetic(self, message, encrypt=False, decrypt=False, key=None):
+    def monoalphabetic(self, message: str, encrypt=False, decrypt=False, key=None):
+        """About - Procedure includes replacing original symbols with random symbols from key.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (dict): Key generated automatically for encryption or decryption.
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
         if encrypt:
-            shuffled_symbols = self.symbols.copy()
+            shuffled_symbols = self.__symbols__.copy()
             random.shuffle(shuffled_symbols)
 
-            key = dict(zip(self.symbols, shuffled_symbols))
+            key = dict(zip(self.__symbols__, shuffled_symbols))
             encrypted_data = []
             for sym in message:
                 encrypted_data.append(key.get(sym))
@@ -233,7 +358,18 @@ class Cypher:
             except Exception as e:
                 raise ValueError("Monoalphabetic cypher can be decrypted with the exact same key generated during encryption")
     
-    def fernet(self, message, encrypt=False, decrypt=False, key=None):
+    def fernet(self, message: str, encrypt=False, decrypt=False, key=None):
+        """About - Procedure includes symmetric cryptographic schemes.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (Fernet object): Key generated automatically for encryption or decryption.
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
         if encrypt:
             key = Fernet.generate_key()
             encrypted_data = Fernet(key).encrypt(message.encode('utf-8')).decode('utf-8')
@@ -249,10 +385,21 @@ class Cypher:
             except Exception as e:
                 raise ValueError("Fernet cypher can be decrypted with the exact same key generated during encryption")
 
-    def onetimepad(self, message, encrypt=False, decrypt=False, key=None):
+    def onetimepad(self, message: str, encrypt=False, decrypt=False, key=None):
+        """About - Procedure includes modifying xor cypher with base64 encoding scheme.
+
+        Args:
+            message (str): message to be encrypted/decrypted.
+            encrypt (bool, optional): Mode of operation. Defaults to False.
+            decrypt (bool, optional): Mode of operation. Defaults to False.
+            key (str): <str> type key required for encryption or decryption. Defaults to random passkey.
+
+        Returns:
+            tuple: encrypted/decrypted data , key
+        """
         if (not key) or (type(key) != str):
             warnings.warn("str based key is preferred for onetimepad cypher. Assuming random key.")
-            key = ''.join([random.choice(self.symbols) for _ in range(len(message))])
+            key = ''.join([random.choice(self.__symbols__) for _ in range(len(message))])
 
         if encrypt:
             data = ''.join([chr(ord(x)^ord(y)) for x, y in zip(message, cycle(key))])
@@ -263,5 +410,3 @@ class Cypher:
             decrypted_data = ''.join([chr(ord(x)^ord(y)) for x, y in zip(data, cycle(key))])
             return decrypted_data, key
 
-
-    
