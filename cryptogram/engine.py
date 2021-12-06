a966116bff3ac66a2fdbeae34b9d3f434f748231
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-12-06 12:04:40 +0530

    Complete Rail Fecnce Logic

--- cryptogram/engine.py
+++ cryptogram/engine.py
@@ -789,3 +789,52 @@ class Cypher:
                         
             encrypted_data = ''.join(result)
             return (encrypted_data, int(key))
+        
+        if decrypt:
+            rail = [['\n' for _ in range(len(message))] for _ in range(key)]
+            
+            dir_down = None
+            row, col = 0, 0
+     
+            # mark the places with '*'
+            for i in range(len(message)):
+                if row == 0:
+                    dir_down = True
+                if row == key - 1:
+                    dir_down = False
+                
+                rail[row][col] = '*'
+                col += 1
+                
+                if dir_down:
+                    row += 1
+                else:
+                    row -= 1
+                    
+            index = 0
+            for i in range(key):
+                for j in range(len(message)):
+                    if ((rail[i][j] == '*') and
+                    (index < len(message))):
+                        rail[i][j] = message[index]
+                        index += 1
+                
+            result = []
+            row, col = 0, 0
+            for i in range(len(message)):
+                
+                if row == 0:
+                    dir_down = True
+                if row == key-1:
+                    dir_down = False
+                    
+                if (rail[row][col] != '*'):
+                    result.append(rail[row][col])
+                    col += 1
+                    
+                if dir_down:
+                    row += 1
+                else:
+                    row -= 1
+            
+            return("".join(result), int(key))
