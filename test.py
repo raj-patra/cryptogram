04d0aa283b41331eec3bdd76447d46421bd53183
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-12-12 10:38:12 +0530

    Added testcases

--- test.py
+++ test.py
@@ -20,4 +20,10 @@ class TestCryptogram(unittest.TestCase):
         encoded = self.enc_obj.encode(message=self.message, engine="base64")
         decoded = self.enc_obj.decode(message=encoded["encoded_message"], engine="base64")
         self.assertEqual(decoded["decoded_message"], self.message)
+    
+    def test_encode_base32(self):
+        
+        encoded = self.enc_obj.encode(message=self.message, engine="base32")
+        decoded = self.enc_obj.decode(message=encoded["encoded_message"], engine="base32")
+        self.assertEqual(decoded["decoded_message"], self.message)
 
