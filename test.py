9a0a3762f8280129e61632653ab558bb82689a45
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2021-12-12 10:37:06 +0530

    Added testcases

---
+++ test.py
@@ -0,0 +1,23 @@
+from base64 import decode, encode
+from cryptogram.cypher import Cypher
+from cryptogram.transform import Transform
+from cryptogram.encode import Encode
+
+import unittest
+
+
+class TestCryptogram(unittest.TestCase):
+    def setUp(self) -> None:
+        self.message = "Hello World!"
+        self.enc_obj = Encode()
+        self.trf_obj = Transform()
+        self.cyp_obj = Cypher()
+        
+        return super().setUp()
+    
+    def test_encode_base64(self):
+        
+        encoded = self.enc_obj.encode(message=self.message, engine="base64")
+        decoded = self.enc_obj.decode(message=encoded["encoded_message"], engine="base64")
+        self.assertEqual(decoded["decoded_message"], self.message)
+
