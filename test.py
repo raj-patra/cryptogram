c7f6c70367be898a296bec990b672b9db9521548
Raj Kishore Patra <rajpatra.kishore@gmail.com>
2022-02-02 11:20:59 +0530

    Added testcases for cypher/transposition

--- test.py
+++ test.py
@@ -193,3 +193,7 @@ class TestCryptogram(unittest.TestCase):
         encrypted = self.cyp_obj.encrypt(message=self.message, engine="transposition", key=4)
         decrypted = self.cyp_obj.decrypt(message=encrypted["encrypted_message"], engine=encrypted["engine"], key=encrypted["key"])
         self.assertEqual(decrypted["decrypted_message"], self.message)
+        
+        encrypted = self.cyp_obj.encrypt(message=self.message, engine="transposition", key="hello")
+        decrypted = self.cyp_obj.decrypt(message=encrypted["encrypted_message"], engine=encrypted["engine"], key=encrypted["key"])
+        self.assertEqual(decrypted["decrypted_message"], self.message)
